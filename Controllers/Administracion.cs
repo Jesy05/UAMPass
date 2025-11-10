24b481a084c153014455b0420a3cecd4d2fb251e
Jesy05 <jgonzalezj@uamv.edu.ni>
2025-11-10 10:00:38 -0600

    drafts de los otros dos portales

---
+++ Controllers/Administracion.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UAMPass.Controllers
+{
+    public class AdministracionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
